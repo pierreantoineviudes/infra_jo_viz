c3edc61104e8a9944ac4ac93b38b339474f223a3
Your Name <pierre.antoine.viudes@gmail.com>
2024-02-12 17:25:03 +0100

    few changes for wordcloud

--- src/testCloud.js
+++ src/testCloud.js
@@ -6,8 +6,8 @@ async function main () {
 
   // set the dimensions and margins of the graph
   const margin = { top: 10, right: 10, bottom: 10, left: 10 }
-  const width = 450 - margin.left - margin.right
-  const height = 450 - margin.top - margin.bottom
+  const width = window.innerWidth - margin.left - margin.right
+  const height = window.innerHeight - margin.top - margin.bottom
 
   // append the svg object to the body of the page
   const svg = d3.select('body').append('svg')
@@ -17,16 +17,51 @@ async function main () {
     .attr('transform',
       'translate(' + margin.left + ',' + margin.top + ')')
 
+  const data = await loadData()
+  // console.log('data : ', data.map(e => {
+  //   const r = {
+  //     sport: e.SPORTS,
+  //     capacite: e.capacite
+  //   }
+  //   return r
+  // }))
+  const dataClean = data.map(e => {
+    const r = {
+      sport: e.SPORTS,
+      capacite: e.capacite
+    }
+    return r
+  })
+  // console.log('data clean : ', dataClean)
+  const rolledupdata = [...d3.rollup(dataClean, v => d3.sum(v, e => e.capacite), d => d.sport)]
+  // console.log('rolledupdata : ', rolledupdata)
+  // const words = data.map(d => { return (d.SPORTS, d.capacite) })
+  // console.log('words : ', words)
+
+  console.log('min : ', d3.min(rolledupdata, d => d[1]))
+  console.log('max : ', d3.max(rolledupdata, d => d[1]))
+
+  const wordCloudScale = d3.scaleLinear()
+    .domain([d3.min(rolledupdata, d => d[1]), d3.max(rolledupdata, d => d[1])])
+    .range([0, 50])
+
   // Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
   const layout = d3.layout.cloud()
     .size([width, height])
-    .words(myWords.map(function (d) { return { text: d, size: Math.random() * 100 } }))
+    .words(rolledupdata.map(function (d) {
+      // console.log('d : ', d)
+      return {
+        text: d[0],
+        size: wordCloudScale(d[1])
+      }
+    }))
     .padding(10)
-    .fontSize(function (d) { return d.size })
+    .fontSize(function (d) {
+      console.log('d : fontsize', d)
+      return d.size
+    })
     .on('end', draw)
   layout.start()
-
-  const data = await loadData()
 
   // This function takes the output of 'layout' above and draw the words
   // Better not to touch it. To change parameters, play with the 'layout' variable above
@@ -48,13 +83,10 @@ async function main () {
   async function loadData () {
     const planningParsed = await (d3.csv('session_planning_with_loc_v3.csv')
       .then(data => {
-        console.log(data)
+        // console.log(data)
         return data.map((d, i) => {
           const r = d
-          r.time = parseDateHour(d.date + ' ' + '2024' + ' ' + d.debut_epreuve)
-          r.num_jour = +r.num_jour
-          r.latitude = +r.latitude
-          r.longitude = +r.longitude
+          r.capacite = +r.capacite
           return r
         })
       }))
